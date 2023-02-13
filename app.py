3d7617158cedbc770c64637de2f595c27b4a7b78
Vince Trost <vincetrost@protonmail.com>
2023-02-13 14:28:03 -0500

    feat: context saved successfully to global var

--- app.py
+++ app.py
@@ -1,39 +1,75 @@
-from chain import load_chain, chat
-import discord
+import collections
 import os
+from typing import Optional
 
-chain = load_chain()
-history = []
+import discord
+from discord.ext import commands
+
+from dotenv import load_dotenv
+from chain import load_chain, chat
 
 
+load_dotenv()
 token = os.environ['BOT_TOKEN']
-# class Client(discord.Client):
-#     async def on_ready(self):
-#         print(f'Logged on as {self.user}!')
-# 
-#     async def on_message(self, message):
-#         print(f'Message from {message.author}: {message.content}')
+
+K=15  # create a constants file so we can ref this in chain.py too
+chain = load_chain()
+history = collections.deque(maxlen=K)
+
+CONTEXT = None
 
 intents = discord.Intents.default()
+intents.messages = True
 intents.message_content = True
 
-client = discord.Client(intents=intents)
+bot = discord.Bot(intents=intents)
 
-@client.event
+@bot.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+    print(f"We have logged in as {bot.user}")
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
+
+@bot.command(description="Set the context for the tutor")
+async def context(ctx, text: Optional[str] = None):
+    global CONTEXT
+    if text is None:
+        if CONTEXT is not None:
+            await ctx.respond(f"Current context: {CONTEXT}")
+            return
+        else:
+            await ctx.respond(f"You never set a context! Add some text after the `/context` command :) ")
+            return
+    CONTEXT = text
+    print(f"Context set to: {CONTEXT}")
+    await ctx.respond("The context has been successfully set!")
+
+
+@bot.event
+async def on_message(ctx, message: Optional[discord.Message] = None):
+    if message.author == ctx.user:
         return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+    print("We're listening...")
+    
+    history.append(message.content)
 
-    if message.content.startswith('$context'):
-        await message.channel.send(f'Context Saved: {message.content[8:]}')
+    if message.content.startswith('@tutor-gpt'):
+        if CONTEXT is None:
+            await message.channel.send('Please set a context using `/context`')
+        response = await chat(CONTEXT, message.content[10:], history, chain)
+        await message.channel.send(response)
 
-client.run(token)
+    print(f'Message from {ctx.author}: {message.content}')
+    await bot.process_commands(message)
 
- 
+# @bot.command(description="Chat with the tutor")
+# async def chat(ctx, message: discord.Message):
+#     if CONTEXT is None:
+#         await ctx.respond("Please set a context using `/context`.")
+#         return
+
+#     history.append(message.content)
+#     response = await chat(CONTEXT, message.content[10:], history, chain)
+#     await ctx.respond(response)
+
+bot.run(token)
