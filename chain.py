f8f478b2e6302ee1361517114d3a952a916746b2
Vince Trost <vincetrost@protonmail.com>
2023-02-13 14:27:42 -0500

    fix: remove gradio code

--- chain.py
+++ chain.py
@@ -1,7 +1,6 @@
 import os
 from typing import Optional, Tuple
 
-import gradio as gr
 import pandas as pd
 from langchain import LLMChain
 from langchain.chains.conversation.memory import ConversationalBufferWindowMemory
@@ -33,16 +32,6 @@ def load_chain():
     )
     return chain
 
-def set_openai_api_key(api_key: str):
-    """Set the api key and return chain.
-    If no api_key, then None is returned.
-    """
-    if api_key:
-        os.environ["OPENAI_API_KEY"] = api_key
-        chain = load_chain()
-        os.environ["OPENAI_API_KEY"] = ""
-        return chain
-
 
 def chat(
     context: str, inp: str, history: Optional[Tuple[str, str]], chain: Optional[LLMChain] 
@@ -65,72 +54,3 @@ def chat(
     return history, history
 
 
-callback = gr.CSVLogger()
-
-with gr.Blocks() as demo:
-    with gr.Row():
-        gr.Markdown("<h3><center>Tutor GPT v1 Demo</center></h3>")
-        openai_api_key_textbox = gr.Textbox(
-            placeholder="Paste your OpenAI API key (sk-...)",
-            show_label=False,
-            lines=1,
-            type="password",
-        )
-
-    with gr.Row():
-        with gr.Column(scale=0.70):
-            context = gr.Textbox(
-                label="Context",
-                placeholder="Paste your excerpt/article/document here...",
-                lines=15,
-            )
-        with gr.Column(scale=0.30):
-            chatbot = gr.Chatbot()
-            btn = gr.Button("💎 GEM 💎").style(full_width=False)
-
-    with gr.Row():
-        message = gr.Textbox(
-            label="What's your question?",
-            placeholder="What's the answer to life, the universe, and everything?",
-            lines=1,
-        )
-        submit = gr.Button(value="Send", variant="secondary").style(full_width=False)
-
-    gr.Examples(
-        examples=[
-            "I don't get it. Can you summarize this for me?",
-            "What's the main idea with this passage?",
-        ],
-        inputs=message,
-    )
-
-    gr.HTML("Demo application of a LangChain chain.")
-
-    gr.HTML(
-        "<center>Powered by <a href='https://github.com/hwchase17/langchain'>LangChain 🦜️🔗</a></center>"
-    )
-
-    state = gr.State()
-    agent_state = gr.State()
-
-    # This needs to be called at some point prior to the first call to callback.flag()
-    callback.setup([context, message, chatbot], "data/flagged_data_points")
-
-    
-    submit.click(chat, inputs=[context, message, state, agent_state], outputs=[chatbot, state])
-    message.submit(chat, inputs=[context, message, state, agent_state], outputs=[chatbot, state])  # same thing, but for hitting <enter>
-
-    # collects gems
-    btn.click(lambda *args: callback.flag(args), [context, message, chatbot], None, preprocess=False)
-
-
-    openai_api_key_textbox.change(
-        set_openai_api_key,
-        inputs=[openai_api_key_textbox],
-        outputs=[agent_state],
-    )
-
-
-
-# demo.launch(debug=True, server_name="0.0.0.0")
-#demo.launch(debug=True)
