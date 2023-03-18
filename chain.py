4322111f992e6848255faaf3482063bad06489f0
Vineeth Voruganti <13438633+VVoruganti@users.noreply.github.com>
2023-03-17 19:03:48 -0700

    Wrapper class on methods to create multiple instances of Chains for each Channel

--- chain.py
+++ chain.py
@@ -26,7 +26,6 @@ RESPONSE_PROMPT_TEMPLATE = load_prompt("data/prompts/response_prompt.yaml")
 THOUGHT_SUMMARY_TEMPLATE = load_prompt("data/prompts/thought_summary_prompt.yaml")
 RESPONSE_SUMMARY_TEMPLATE = load_prompt("data/prompts/response_summary_prompt.yaml")
 
-session_cache = {}
 
 def load_chains():
     """Logic for loading the chain you want to use should go here."""
@@ -117,4 +116,89 @@ async def chat(
 
     return response, thought
 
+class ChannelChain:
+    def __init__(self):
+        self.thought_chain = None
+        self.response_chain = None
+        self.restart()
+        
+
+    def restart(self):
+        llm = OpenAI(temperature=0.9)
+        llm_thought_summary = OpenAI(max_tokens=75)  # how long we want our academic needs list to be
+        llm_response_summary = OpenAI(max_tokens=150) # how long we want our dialogue summary to be
+        self.thought_chain = LLMChain(
+            llm=llm, 
+            memory=ConversationSummaryBufferMemory(
+                prompt=THOUGHT_SUMMARY_TEMPLATE,
+                max_token_limit=100,  # how much of the history we're trying to summarize
+                llm=llm_thought_summary,
+                memory_key="history",   # when you have multiple inputs, you need to specify which inputs to record for history
+                input_key="input",
+                ai_prefix="Thought",
+                human_prefix="Student"
+            ), 
+            prompt=THOUGHT_PROMPT_TEMPLATE, 
+            verbose=False
+            # TODO change back to True
+        )
+
+        self.response_chain = LLMChain(
+            llm=llm, 
+            memory=ConversationSummaryBufferMemory(
+                prompt=RESPONSE_SUMMARY_TEMPLATE,
+                max_token_limit=100, 
+                llm=llm_response_summary,
+                memory_key="history",   # when you have multiple inputs, you need to specify which inputs to record for history
+                input_key="input",
+                ai_prefix="Tutor",
+                human_prefix="Student"
+            ), 
+            prompt=RESPONSE_PROMPT_TEMPLATE, 
+            verbose=False
+            # TODO change back to True
+        )
+
+    async def chat(self, context: str, inp: str):
+        """Execute the chat functionality."""
+        # history = history or []
+        
+        # If chain is None, that is because no API key was provided.
+        if self.thought_chain is None:
+            print("Please set your OpenAI key to use")
+            return
+        if self.response_chain is None:
+            print("Please set your OpenAI key to use")
+            return
+
+        # Run chains and append input.
+        try:
+            thought = self.thought_chain.predict(
+                context=context, 
+                input=inp
+            )
+            if 'Tutor:' in thought:
+                thought = thought.split('Tutor:')[0].strip()
+        except Exception as e:
+            rollbar.report_exc_info()
+            thought = str(e)
+
+        try:
+            response = self.response_chain.predict(
+                context=context,
+                input=inp,
+                thought=thought
+            )
+            if 'Student:' in response:
+                response = response.split('Student:')[0].strip()
+            if 'Studen:' in response:  # this happened once: https://discord.com/channels/1016845111637839922/1073429619639853066/1080233497073025065
+                response = response.split('Studen:')[0].strip()
+        except Exception as e:
+            rollbar.report_exc_info()
+            response = str(e)
+
+        # log these things
+        # history.append((inp, thought, response))
+
+        return response, thought
 
