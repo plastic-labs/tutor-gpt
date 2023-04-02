3a53da208785f35c727bb9d524ee439e1ea74d96
vintro <vince@plasticlabs.ai>
2023-04-02 15:28:26 -0400

    feat: workshop and discuss commands

--- bot/context.py
+++ bot/context.py
@@ -1,5 +1,4 @@
 import discord
-import asyncio
 import time
 import globals
 from typing import Optional
@@ -11,22 +10,19 @@ class Context(commands.Cog):
     def __init__(self, bot) -> None:
         self.bot = bot
 
-    @commands.slash_command(description="Set the context for the tutor or show it")
+
+    @commands.slash_command(description="Set the context for Bloom or show it")
     async def context(self, ctx: discord.ApplicationContext, text: Optional[str] = None):
         """
-        This function sets the context global var for the tutor to engage in discussion on.
-
-        Args:
-            ctx: context, necessary for bot commands
-            text: the passage (we're also calling it "CONTEXT") to be injected into the prompt
+        This function directs the user to the appropriate activity to engage in with the text they provided.
         """
         LOCAL_CHAIN = globals.CACHE.get(ctx.channel_id)
         if text is None:
             # no text given, show current text to user or let them know nothing's been set
             if LOCAL_CHAIN is not None and LOCAL_CHAIN.context is not None:
-                await ctx.respond(f"Current context: {LOCAL_CHAIN.context}", ephemeral=True)
+                await ctx.respond(f"*Current context:* {LOCAL_CHAIN.context}", ephemeral=True)
             else:
-                await ctx.respond(f"You never set a context! Add some text after the `/context` command :) ")
+                await ctx.respond(f"*Bloom never got any text! Paste some text after the* `/context` *command*")
         else:
             # text given, assign or update the context
             if LOCAL_CHAIN is not None and LOCAL_CHAIN.context is not None:
@@ -45,13 +41,103 @@ class Context(commands.Cog):
                 end = time.time()
                 print(f"Elapsed: {end - start}")
 
+
+    @commands.slash_command(name="discuss", description="Discuss your text with Bloom")
+    async def discuss(self, ctx: discord.ApplicationContext, text: Optional[str] = None):
+        LOCAL_CHAIN = globals.CACHE.get(ctx.channel_id)
+        if text is None:
+            # no text given, show current text to user or let them know nothing's been set
+            if LOCAL_CHAIN is not None and LOCAL_CHAIN.context is not None:
+                await ctx.respond(f"*Currently discussing:* {LOCAL_CHAIN.context}", ephemeral=True)
+            else:
+                await ctx.respond(f"*Bloom doesn't have anything to discuss! Paste some text after the* `/discuss` *command*")
+        else:
+            # text given, assign or update the context
+            if LOCAL_CHAIN is not None and LOCAL_CHAIN.context is not None:
+                start = time.time()
+                await ctx.response.defer()
+                # updating the context, so restart conversation
+                await ctx.invoke(self.bot.get_command('restart'), respond=False)
+                LOCAL_CHAIN.context = text
+                print(f"*Discussion updated to:* {LOCAL_CHAIN.context}")
+                response = await chat(
+                    context=LOCAL_CHAIN.context,
+                    starter_chain=globals.STARTER_CHAIN
+                )
+                LOCAL_CHAIN.response_memory.chat_memory.add_ai_message(response)
+                await ctx.followup.send(response)
+                end = time.time()
+                print(f"Elapsed: {end - start}")
+            else:
+                # setting context for the first time
+                start = time.time()
+                await ctx.response.defer()
+                # Create new cache entry
+                LOCAL_CHAIN = ConversationCache(text)
+                globals.CACHE.put(ctx.channel_id, LOCAL_CHAIN)
+                print(f"*Discussion set to:* {LOCAL_CHAIN.context}")
+                response = await chat(
+                    context=text,
+                    starter_chain=globals.STARTER_CHAIN
+                )
+                LOCAL_CHAIN.response_memory.chat_memory.add_ai_message(response)
+                await ctx.followup.send(response)
+                end = time.time()
+                print(f"Elapsed: {end - start}")
+
+
+    @commands.slash_command(name="workshop", description="Workshop your writing with Bloom")
+    async def workshop(self, ctx: discord.ApplicationContext, text: Optional[str] = None):
+        LOCAL_CHAIN = globals.CACHE.get(ctx.channel_id)
+        if text is None:
+            # no text given, show current text to user or let them know nothing's been set
+            if LOCAL_CHAIN is not None and LOCAL_CHAIN.context is not None:
+                await ctx.respond(f"*Currently workshopping:* {LOCAL_CHAIN.context}", ephemeral=True)
+            else:
+                await ctx.respond(f"*Bloom doesn't have anything to workshop! Paste some text after the* `/workshop` *command*")
+        else:
+            # text given, assign or update the context
+            if LOCAL_CHAIN is not None and LOCAL_CHAIN.context is not None:
+                start = time.time()
+                await ctx.response.defer()
+                # updating the context, so restart conversation
+                await ctx.invoke(self.bot.get_command('restart'), respond=False)
+                LOCAL_CHAIN.context = text
+                print(f"*Workshop text updated to:* {LOCAL_CHAIN.context}")
+                response = await chat(
+                    context=LOCAL_CHAIN.context,
+                    starter_chain=globals.STARTER_CHAIN
+                )
+                LOCAL_CHAIN.response_memory.chat_memory.add_ai_message(response)
+                await ctx.followup.send(response)
+                end = time.time()
+                print(f"Elapsed: {end - start}")
+            else:
+                # setting context for the first time
+                start = time.time()
+                await ctx.response.defer()
+                # Create new cache entry
+                LOCAL_CHAIN = ConversationCache(text)
+                globals.CACHE.put(ctx.channel_id, LOCAL_CHAIN)
+                print(f"*Workshop text set to:* {LOCAL_CHAIN.context}")
+                response = await chat(
+                    context=text,
+                    starter_chain=globals.STARTER_CHAIN
+                )
+                LOCAL_CHAIN.response_memory.chat_memory.add_ai_message(response)
+                await ctx.followup.send(response)
+                end = time.time()
+                print(f"Elapsed: {end - start}")
+
+
 class ContextView(discord.ui.View):
     def __init__(self, text):
         super().__init__()
         self.text=text
 
-    @discord.ui.button(label="Discuss", row = 0, style=discord.ButtonStyle.primary, emoji="💬")
+    @discord.ui.button(label="Discuss", style=discord.ButtonStyle.primary, emoji="💬")
     async def discuss_button_callback(self, button, interaction):
+        start = time.time()
         await interaction.response.defer()
 
         self.discuss_button_callback.disabled = True
@@ -71,10 +157,14 @@ class ContextView(discord.ui.View):
             )
             LOCAL_CHAIN.response_memory.chat_memory.add_ai_message(response)
             await interaction.followup.send(response)
+        
+        end = time.time()
+        print(f"Elapsed: {end - start}")
 
 
-    @discord.ui.button(label="Workshop", row = 1, style=discord.ButtonStyle.primary, emoji="✍️")
+    @discord.ui.button(label="Workshop", style=discord.ButtonStyle.primary, emoji="✍️")
     async def workshop_button_callback(self, button, interaction):
+        start = time.time()
         await interaction.response.defer()
 
         self.discuss_button_callback.disabled = True
@@ -94,6 +184,9 @@ class ContextView(discord.ui.View):
             )
             LOCAL_CHAIN.response_memory.chat_memory.add_ai_message(response)
             await interaction.followup.send(response)
+        
+        end = time.time()
+        print(f"Elapsed: {end - start}")
 
 
 def setup(bot):
