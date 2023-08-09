d4b9b57c83d7e8b5aa4095976a189ef5cc4e2821
hyusap <paulayush@gmail.com>
2023-08-09 18:32:45 -0400

    add retrieval react chain implemenation

--- agent/chain.py
+++ agent/chain.py
@@ -7,6 +7,14 @@ from langchain.prompts import (
 )
 from langchain.prompts import load_prompt
 from langchain.schema import AIMessage, HumanMessage
+
+from langchain.vectorstores import FAISS
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.schema import Document
+
+from langchain.agents import load_tools, initialize_agent, AgentType
+from langchain.tools import format_tool_to_openai_function
+
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -34,6 +42,13 @@ class BloomChain:
         # setup prompts
         self.system_thought = SystemMessagePromptTemplate(prompt=SYSTEM_THOUGHT)
         self.system_response = SystemMessagePromptTemplate(prompt=SYSTEM_RESPONSE)
+
+        self.tools = load_tools(["google-serper", "wolfram-alpha", "wikipedia", "arxiv"], llm=self.llm)
+        tool_documents = [Document(page_content=tool.description, metadata={"index": index}) for index, tool in enumerate(self.tools)]
+        tool_vectorstore = FAISS.from_documents(tool_documents, OpenAIEmbeddings())
+        self.tool_retriever = tool_vectorstore.as_retriever()
+        self.functions = [format_tool_to_openai_function(tool) for tool in self.tools]
+
         
 
     async def think(self, thought_memory: ChatMessageHistory, input: str) -> str:
@@ -49,12 +64,28 @@ class BloomChain:
 
         return thought_message.content
     
+    async def gather_info(self, response_memory: ChatMessageHistory, input: str, thought: str) -> str:
+        """Collect information via tool."""
+        tool_thought = thought.split("Tooling:")[1].split("Data:")[0].strip() # separate tool information from thought
+        relevant_tools = self.tool_retriever.get_relevant_documents(tool_thought)
+        print("Relevant Tools: ", relevant_tools)
+        actual_tools = [self.tools[tool.metadata["index"]] for tool in relevant_tools]
 
-    async def respond(self, response_memory: ChatMessageHistory, thought: str, input: str) -> str:
+        # agent = initialize_agent(actual_tools, self.llm, AgentType.CHAT_ZERO_SHOT_REACT_DESCRIPTION, verbose=self.verbose)
+        # information = agent.run(f"gather relevant information to this user's question: {input}", fun) 
+        history = "\n".join([f"{message.type}: {message.content}" for message in response_memory.messages])
+        agent = initialize_agent(actual_tools, self.llm, AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=self.verbose)
+        information = agent.run(f"Here is the conversation history: \n```\n{history}\n```\nGather relevant information to this user's question, utilizing the conversation history: {input}")
+        print("Information: ", information)
+
+        return information
+    
+
+    async def respond(self, response_memory: ChatMessageHistory, thought: str, input: str, info: str) -> str:
         """Generate Bloom's response to the user."""
 
         # load message history
-        messages = [self.system_response.format(thought=thought), *response_memory.messages, HumanMessage(content=input)]
+        messages = [self.system_response.format(thought=thought, info=info), *response_memory.messages, HumanMessage(content=input)]
         response_message = await self.llm.apredict_messages(messages)
 
         # update chat memory
@@ -66,7 +97,8 @@ class BloomChain:
 
     async def chat(self, cache: ConversationCache, inp: str ) -> tuple[str, str]:
         thought  = await self.think(cache.thought_memory, inp)
-        response = await self.respond(cache.response_memory, thought, inp)
+        info = await self.gather_info(cache.response_memory, inp, thought)
+        response = await self.respond(cache.response_memory, thought, inp, info)
         return thought, response
 
 
