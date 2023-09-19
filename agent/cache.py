f6335839b415bd4115c78639379feeca9a7f6a32
Vineeth Voruganti <13438633+VVoruganti@users.noreply.github.com>
2023-09-18 22:36:55 -0400

    Optimization (#96)
    * Usable without honcho and move metadata to creation points
    
    * Remove unused psycopg code
    
    * Add Sentry instrumentation
    
    * Sentry to python
    
    * Update env

--- agent/cache.py
+++ agent/cache.py
@@ -7,9 +7,11 @@ from .mediator import SupabaseMediator
 import uuid
 from typing import List, Tuple, Dict
 from langchain.schema import BaseMessage
+import sentry_sdk
 
 class Conversation:
     "Wrapper Class for storing contexts between channels. Using an object to pass by reference avoid additional cache hits"
+    @sentry_sdk.trace
     def __init__(self, mediator: SupabaseMediator, user_id: str, conversation_id: str = str(uuid.uuid4()), location_id: str = "web", metadata: Dict = {}):
         self.mediator: SupabaseMediator = mediator
         self.user_id: str = user_id
@@ -17,15 +19,19 @@ class Conversation:
         self.location_id: str = location_id
         self.metadata: Dict = metadata
 
+    @sentry_sdk.trace
     def add_message(self, message_type: str, message: BaseMessage,) -> None:
         self.mediator.add_message(self.conversation_id, self.user_id, message_type, message)
 
+    @sentry_sdk.trace
     def messages(self, message_type: str, limit: Tuple[bool, int | None] = (True, 10)) -> List[BaseMessage]:
         return self.mediator.messages(self.conversation_id, self.user_id, message_type, limit=limit)
 
+    @sentry_sdk.trace
     def delete(self) -> None:
         self.mediator.delete_conversation(self.conversation_id)
 
+    @sentry_sdk.trace
     def restart(self) -> None:
         self.delete()
         representation = self.mediator.add_conversation(user_id=self.user_id, location_id=self.location_id)
@@ -34,10 +40,12 @@ class Conversation:
 
 
 class LRUCache:
+    @sentry_sdk.trace
     def __init__(self, capacity: int):
         self.capacity = capacity
         self.cache = OrderedDict()
 
+    @sentry_sdk.trace
     def get(self, key: str):
         if key not in self.cache:
             return None
@@ -46,6 +54,7 @@ class LRUCache:
         self.cache.move_to_end(key)
         return self.cache[key]
 
+    @sentry_sdk.trace
     def put(self, key: str, value: Conversation):
         if key in self.cache:
             # If the key already exists, move it to the end and update the value
@@ -60,11 +69,13 @@ class LRUCache:
 
 class LayeredLRUCache:
     """A Conversation LRU Cache that bases keys on the location of a conversation. The assumption is that the location is a unique identifier"""
+    @sentry_sdk.trace
     def __init__(self, capacity, mediator: SupabaseMediator):
         self.capacity = capacity
         self.memory_cache = OrderedDict()
         self.mediator = mediator
 
+    @sentry_sdk.trace
     def get(self, user_id: str, location_id: str) -> None | Conversation:
         key = location_id+user_id
         if key in self.memory_cache:
@@ -80,6 +91,7 @@ class LayeredLRUCache:
             return self.memory_cache[key]
         return None
 
+    @sentry_sdk.trace
     def put(self, user_id: str, location_id: str) -> Conversation:
         # Add the conversation data to the postgres via the mediator
         representation: Dict = self.mediator.add_conversation(location_id=location_id, user_id=user_id)
@@ -93,6 +105,7 @@ class LayeredLRUCache:
         self.memory_cache[key] = Conversation(self.mediator, location_id=location_id, user_id=user_id, conversation_id=conversation_id, metadata=metadata)
         return self.memory_cache[key]
 
+    @sentry_sdk.trace
     def get_or_create(self, user_id: str, location_id: str, restart: bool = False) -> Conversation:
         cache: None | Conversation = self.get(location_id=location_id, user_id=user_id)
         if cache is None:
--- agent/mediator.py
+++ agent/mediator.py
@@ -1,25 +1,21 @@
-from langchain.memory import PostgresChatMessageHistory
 from langchain.schema.messages import BaseMessage, _message_to_dict, messages_from_dict
 import uuid
-import urllib
 import os
+import sentry_sdk
 from dotenv import load_dotenv
-# Pyscopg For Postgres Management
-import psycopg
-from psycopg.rows import dict_row
 # Supabase for Postgres Management
 from supabase.client import create_client, Client
 from typing import List, Tuple, Dict
-import json
-import random
 load_dotenv()
 
 class SupabaseMediator:
+    @sentry_sdk.trace
     def __init__(self):
         self.supabase: Client = create_client(os.environ['SUPABASE_URL'], os.environ['SUPABASE_KEY'])
         self.memory_table = os.environ["MEMORY_TABLE"]
         self.conversation_table = os.environ["CONVERSATION_TABLE"]
 
+    @sentry_sdk.trace
     def messages(self, session_id: str, user_id: str, message_type: str, limit: Tuple[bool, int | None] = (True,10)) -> List[BaseMessage]:  # type: ignore
         query = self.supabase.table(self.memory_table).select("message").eq("session_id", session_id).eq("user_id", user_id).eq("message_type", message_type).order("id", desc=True)
         if limit[0]:
@@ -29,6 +25,7 @@ class SupabaseMediator:
         messages = messages_from_dict(items)
         return messages[::-1]
 
+    @sentry_sdk.trace
     def add_message(self, session_id: str, user_id: str, message_type: str, message: BaseMessage) -> None:
         payload =  {
                  "session_id": session_id, 
@@ -38,6 +35,7 @@ class SupabaseMediator:
                 }
         self.supabase.table(self.memory_table).insert(payload).execute()
 
+    @sentry_sdk.trace
     def conversations(self, location_id: str, user_id: str, single: bool = True) -> List[Dict] | None:
         try:
             response = self.supabase.table(self.conversation_table).select(*["id", "metadata"], count="exact").eq("location_id", location_id).eq("user_id", user_id).eq("isActive", True).order("created_at", desc=True).execute()
@@ -57,23 +55,26 @@ class SupabaseMediator:
             return None
 
 
+    @sentry_sdk.trace
     def conversation(self, session_id: str) -> Dict | None:
         response = self.supabase.table(self.conversation_table).select("*").eq("id", session_id).eq("isActive", True).maybe_single().execute()
         if response:
            return response.data
         return None
 
+    @sentry_sdk.trace
     def _cleanup_conversations(self, conversation_ids: List[str]) -> None:
         for conversation_id in conversation_ids:
             self.supabase.table(self.conversation_table).update({"isActive": False}).eq("id", conversation_id).execute()
     
-    def add_conversation(self, location_id: str, user_id: str) -> Dict:
+    @sentry_sdk.trace
+    def add_conversation(self, location_id: str, user_id: str, metadata: Dict = {}) -> Dict:
         conversation_id = str(uuid.uuid4())
         payload = {
                 "id": conversation_id, 
                 "user_id": user_id, 
                 "location_id": location_id,
-                "metadata": {"A/B": bool(random.getrandbits(1))}
+                "metadata": metadata,
         }
         representation = self.supabase.table(self.conversation_table).insert(payload, returning="representation").execute() # type: ignore
         print("========================================")
@@ -81,9 +82,11 @@ class SupabaseMediator:
         print("========================================")
         return representation.data[0]
 
+    @sentry_sdk.trace
     def delete_conversation(self, conversation_id: str) -> None:
         self.supabase.table(self.conversation_table).update({"isActive": False}).eq("id", conversation_id).execute()
 
+    @sentry_sdk.trace
     def update_conversation(self, conversation_id: str, metadata: Dict) -> None:
        cur =  self.supabase.table(self.conversation_table).select("metadata").eq("id", conversation_id).single().execute()
        if cur.data['metadata'] is not None:
@@ -93,110 +96,3 @@ class SupabaseMediator:
            new_metadata = metadata
        self.supabase.table(self.conversation_table).update({"metadata": new_metadata}, returning="representation").eq("id", conversation_id).execute() # type: ignore
 
-
-# Modification of PostgresChatMessageHistory: https://api.python.langchain.com/en/latest/_modules/langchain/memory/chat_message_histories/postgres.html#PostgresChatMessageHistory
-class PostgresMediator:
-    """
-    Wrapper class for encapsulating the multiple different chains used in reasoning for the tutor's thoughts
-
-    This is a modified version of the PostgresChatMessageHistory class from langchain.memory.chat_message_histories.postgres
-    It has a few different nuances and goals
-    * For each message the following attributes should be encapsulated
-        * ~session_id
-        * ~user_id
-        * ~message_content
-        * ~sequential id
-        * ~message_type - should be an enum corresponding to the different tags we've been using via langsmith
-        * ~timestamp
-    There should only really be one connection at a time so this can be thought of more as a singleton used to interface with postgres
-
-    Can send everything to the same table
-
-    """
-    def __init__(self, table_name = "message_store"):
-        try:
-            connection_string = urllib.parse.quote(os.environ["SUPABASE_CONNECTION_URL"], safe='/:@', encoding=None, errors=None) # type: ignore
-            self.connection: psycopg.Connection = psycopg.connect(connection_string)
-            self.cursor: psycopg.Cursor = self.connection.cursor(row_factory=dict_row)
-        except psycopg.OperationalError as error:
-            pass
-        
-        self.table_name = table_name
-        self._create_table_if_not_exists()
-            # session_id=str(uuid.uuid4())
-    def _create_table_if_not_exists(self) -> None:
-        create_table_query = f"""CREATE TABLE IF NOT EXISTS {self.table_name} (
-            id SERIAL PRIMARY KEY,
-            session_id TEXT NOT NULL,
-            user_id TEXT NOT NULL,
-            message_type TEXT NOT NULL,
-            timestamp TIMESTAMPTZ NOT NULL DEFAULT now(),
-            message JSONB NOT NULL
-        );"""
-        self.cursor.execute(create_table_query)
-        self.connection.commit()
-
-    
-    def messages(self, session_id: str, user_id: str, message_type: str) -> List[BaseMessage]:  # type: ignore
-        """Retrieve the messages from PostgreSQL"""
-        query = (
-            f"SELECT message FROM {self.table_name} WHERE session_id = %s AND user_id = %s AND message_type = %s ORDER BY id;"
-        )
-        messages = []
-        try:
-            self.cursor.execute(query, (session_id, user_id, message_type))
-            items = [record["message"] for record in self.cursor.fetchall()]
-            messages = messages_from_dict(items)
-        except Exception as e:
-            print(e)
-        return messages
-
-    def add_message(self, session_id: str, user_id: str, message_type: str, message: BaseMessage) -> None:
-        """Append the message to the record in PostgreSQL"""
-        from psycopg import sql
-
-        query = sql.SQL("INSERT INTO {} (session_id, user_id, message_type, message) VALUES (%s, %s, %s, %s);").format(
-            sql.Identifier(self.table_name)
-        )
-        try:
-            self.cursor.execute(
-                query, (session_id, user_id, message_type, json.dumps(_message_to_dict(message)))
-            )
-            self.connection.commit()
-        except Exception as e:
-            print(e)
-
-    # def clear(self) -> None:
-    #     """Clear session memory from PostgreSQL"""
-    #     query = f"DELETE FROM {self.table_name} WHERE session_id = %s;"
-    #     self.cursor.execute(query, (self.session_id,))
-    #     self.connection.commit()
-
-    def __del__(self) -> None:
-        if self.cursor:
-            self.cursor.close()
-        if self.connection:
-            self.connection.close()
-
-
-class ConversationCache:
-    "Wrapper Class for storing contexts between channels. Using an object to pass by reference avoid additional cache hits"
-    def __init__(self):
-        # Eventually could mix this with a summary memory object: https://python.langchain.com/docs/modules/memory/agent_with_memory_in_db
-        # self.thought_history = PostgresChatMessageHistory()
-        # self.response_history = PostgresChatMessageHistory()
-        self.conversation_id: str = str(uuid.uuid4())
-        # Note the importance of escaping for the connection string make sure that "/:@" are all considered safe
-        self.thought_memory: PostgresChatMessageHistory = PostgresChatMessageHistory(
-            connection_string=urllib.parse.quote(os.environ["POSTGRES_URL"], safe='/:@', encoding=None, errors=None),
-            session_id=self.conversation_id
-        )
-        self.response_memory: PostgresChatMessageHistory = PostgresChatMessageHistory(
-            connection_string=urllib.parse.quote(os.environ["POSTGRES_URL"], safe='/:@', encoding=None, errors=None),
-            session_id=self.conversation_id
-        )
-
-    def restart(self):
-        self.conversation_id: str = str(uuid.uuid4())
-        self.thought_memory.session_id = self.conversation_id
-        self.response_memory.session_id = self.conversation_id
