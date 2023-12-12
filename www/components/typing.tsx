d52002a60c32ade0e10ba38557a537a2639e3e80
hyusap <paulayush@gmail.com>
2023-12-12 12:47:07 -0500

    💄 fix dark mode typing indicator

--- www/components/typing.tsx
+++ www/components/typing.tsx
@@ -1,6 +1,6 @@
 export default function Typing() {
   return (
-    <div className="typing_indicator flex flex-row gap-2 h-7 px-3 rounded-full bg-gray-100 justify-center items-center w-min">
+    <div className="typing_indicator flex flex-row gap-2 h-7 px-3 rounded-full bg-gray-100 justify-center items-center w-min dark:bg-gray-800">
       <div className="typing_dot bg-gray-500 rounded-full h-2 w-2"></div>
       <div className="typing_dot bg-gray-500 rounded-full h-2 w-2"></div>
       <div className="typing_dot bg-gray-500 rounded-full h-2 w-2"></div>
