28d81f3d9650b59bdeab3e8ebdf2fb141e92d272
vintro <vince@plasticlabs.ai>
2024-10-21 15:54:42 -0400

    functioning, some issues displaying thoughts

--- api/routers/chat.py
+++ api/routers/chat.py
@@ -6,6 +6,9 @@ from api.dependencies import app, honcho
 
 from agent.chain import ThinkCall, RespondCall
 
+import os
+
+
 router = APIRouter(prefix="/api", tags=["chat"])
 
 
@@ -16,7 +19,7 @@ async def stream(
     """Stream the response too the user, currently only used by the Web UI and has integration to be able to use Honcho is not anonymous"""
     user = honcho.apps.users.get_or_create(app_id=app.id, name=inp.user_id)
 
-    def convo_turn():
+    async def convo_turn():
         thought_stream = ThinkCall(
             user_input=inp.message,
             app_id=app.id,
@@ -29,6 +32,16 @@ async def stream(
             thought += chunk
             yield chunk
 
+        # dialectic
+        honcho_response = honcho.apps.users.sessions.chat(
+            app_id=app.id,
+            user_id=user.id,
+            session_id=str(inp.conversation_id),
+            queries=thought,
+        )
+        honcho_content = honcho_response.content
+
+
         yield "❀"
         response_stream = RespondCall(
             user_input=inp.message,
@@ -37,6 +50,7 @@ async def stream(
             user_id=user.id,
             session_id=str(inp.conversation_id),
             honcho=honcho,
+            honcho_content=honcho_content,
         ).stream()
         response = ""
         for chunk in response_stream:
@@ -44,27 +58,49 @@ async def stream(
             yield chunk
         yield "❀"
 
-        honcho.apps.users.sessions.messages.create(
+        user_message = honcho.apps.users.sessions.messages.create(
             is_user=True,
             session_id=str(inp.conversation_id),
             app_id=app.id,
             user_id=user.id,
             content=inp.message,
         )
-        new_ai_message = honcho.apps.users.sessions.messages.create(
-            is_user=False,
+        thought_metamessage = f"""<honcho-response>{honcho_content}</honcho-response>\n<bloom>{response}</bloom>\n{inp.message}"""
+        honcho.apps.users.sessions.metamessages.create(
+            app_id=app.id,
+            user_id=user.id,
+            session_id=str(inp.conversation_id),
+            message_id=user_message.id,
+            metamessage_type="thought",
+            content=thought_metamessage,
+            metadata={"type": "user"}
+        )
+        # save thought (honcho query) as metamessage
+        honcho.apps.users.sessions.metamessages.create(
+            session_id=str(inp.conversation_id),
+            app_id=app.id,
+            user_id=user.id,
+            content=f"<honcho>{honcho_content}</honcho>",
+            message_id=user_message.id,
+            metamessage_type="thought",
+            metadata={"type": "assistant"}
+        )
+        # save bloom's response
+        bloom_message = honcho.apps.users.sessions.messages.create(
             session_id=str(inp.conversation_id),
             app_id=app.id,
             user_id=user.id,
             content=response,
+            is_user=False,
         )
+        response_metamessage = f"""<honcho-response>{honcho_content}</honcho-response>\n{inp.message}"""
         honcho.apps.users.sessions.metamessages.create(
             app_id=app.id,
-            session_id=str(inp.conversation_id),
             user_id=user.id,
-            message_id=new_ai_message.id,
-            metamessage_type="thought",
-            content=thought,
+            session_id=str(inp.conversation_id),
+            message_id=bloom_message.id,
+            metamessage_type="response",
+            content=response_metamessage,
         )
     return StreamingResponse(convo_turn())
 
