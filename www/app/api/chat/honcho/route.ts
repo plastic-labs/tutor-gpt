d3c5b7b146505063ef13bb90fa989e214a350e5f
Ayush Paul <paulayush@gmail.com>
2024-12-26 15:28:53 -0500

    Prompt and route refactor (#192)
    * streaming refactor
    
    * improve prompting framework
    
    * more refactoring
    
    * update prompts to match main
    
    * fix (chat): Force dynamic for vercel
    
    ---------
    
    Co-authored-by: Vineeth Voruganti <13438633+VVoruganti@users.noreply.github.com>

---
+++ www/app/api/chat/honcho/route.ts
@@ -0,0 +1,28 @@
+import { getUserData } from '@/utils/ai';
+import { honcho } from '@/utils/honcho';
+import { NextRequest, NextResponse } from 'next/server';
+
+export const runtime = 'nodejs';
+export const maxDuration = 100;
+export const dynamic = 'force-dynamic'; // always run dynamically
+
+export async function POST(req: NextRequest) {
+  const { message, conversationId } = await req.json();
+
+  const userData = await getUserData();
+
+  if (!userData) {
+    return new Response('Unauthorized', { status: 401 });
+  }
+
+  const { appId, userId } = userData;
+
+  const dialecticQuery = await honcho.apps.users.sessions.chat(
+    appId,
+    userId,
+    conversationId,
+    { queries: message }
+  );
+
+  return NextResponse.json({ content: dialecticQuery.content });
+}
--- www/utils/ai.ts
+++ www/utils/ai.ts
@@ -1,15 +1,15 @@
+import { getHonchoApp, getHonchoUser, honcho } from '@/utils/honcho';
 import { createClient } from '@/utils/supabase/server';
-import { NextRequest, NextResponse } from 'next/server';
-import { thinkCall, respondCall } from './actions';
-import { honcho, getHonchoApp, getHonchoUser } from '@/utils/honcho';
-import { streamText } from 'ai';
 import { createOpenRouter } from '@openrouter/ai-sdk-provider';
+import { streamText } from 'ai';
+import d from 'dedent-js';
 
 import * as Sentry from '@sentry/nextjs';
 
-export const runtime = 'nodejs';
-export const maxDuration = 100;
-export const dynamic = 'force-dynamic'; // always run dynamically
+export interface Message {
+  role: 'user' | 'assistant';
+  content: string;
+}
 
 const OPENROUTER_API_KEY = process.env.AI_API_KEY;
 const MODEL = process.env.MODEL || 'gpt-3.5-turbo';
@@ -32,6 +32,19 @@ const openrouter = createOpenRouter({
   },
 });
 
+export interface HistoryWithoutResponse {
+  appId: string;
+  userId: string;
+  sessionId: string;
+  userInput: string;
+  thought?: string;
+  honchoContent?: string;
+}
+
+type History = HistoryWithoutResponse & {
+  aiResponse: string;
+};
+
 async function saveHistory({
   appId,
   userId,
@@ -40,15 +53,7 @@ async function saveHistory({
   thought,
   honchoContent,
   aiResponse,
-}: {
-  appId: string;
-  userId: string;
-  sessionId: string;
-  userInput: string;
-  thought: string;
-  honchoContent: string;
-  aiResponse: string;
-}) {
+}: History) {
   try {
     // Create user message
     const newUserMessage = await honcho.apps.users.sessions.messages.create(
@@ -92,7 +97,7 @@ async function saveHistory({
       userId,
       sessionId,
       {
-        content: thought,
+        content: thought || '',
         message_id: newAiMessage.id,
         metamessage_type: 'thought',
         metadata: { type: 'assistant' },
@@ -117,9 +122,56 @@ async function saveHistory({
   }
 }
 
-async function fetchOpenRouter(type: string, messages: any[], payload: any) {
-  try {
+export async function getUserData() {
+  const supabase = createClient();
 
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    return null;
+  }
+
+  const honchoApp = await getHonchoApp();
+  const honchoUser = await getHonchoUser(user.id);
+
+  return {
+    appId: honchoApp.id,
+    userId: honchoUser.id,
+  };
+}
+
+export const user = (
+  strings: TemplateStringsArray,
+  ...values: unknown[]
+): Message => ({
+  role: 'user',
+  content: d(strings, ...values),
+});
+
+export const assistant = (
+  strings: TemplateStringsArray,
+  ...values: unknown[]
+): Message => ({
+  role: 'assistant',
+  content: d(strings, ...values),
+});
+
+// export const system = (
+//   strings: TemplateStringsArray,
+//   ...values: unknown[]
+// ): Message => ({
+//   role: 'system',
+//   content: d(strings, ...values),
+// });
+
+export async function createStream(
+  type: string,
+  messages: Message[],
+  payload: HistoryWithoutResponse
+) {
+  try {
     const result = streamText({
       model: openrouter(MODEL),
       messages,
@@ -137,99 +189,14 @@ async function fetchOpenRouter(type: string, messages: any[], payload: any) {
           userId: payload.userId,
           release: SENTRY_RELEASE,
           environment: SENTRY_ENVIRONMENT,
-          tags: [type]
-        }
-      }
+          tags: [type],
+        },
+      },
     });
     return result.toTextStreamResponse();
   } catch (error) {
     console.error('Error in fetchOpenRouter:', error);
     Sentry.captureException(error);
     throw error;
-  }
-}
-
-export async function POST(req: NextRequest) {
-  const supabase = createClient();
-
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
-
-  if (!user) {
-    return new NextResponse('Unauthorized', { status: 401 });
-  }
-
-  const data = await req.json();
-
-  const { type, message, conversationId, thought, honchoThought } = data;
-
-  console.log('Starting Stream');
-
-  const honchoApp = await getHonchoApp();
-  const honchoUser = await getHonchoUser(user.id);
-
-  console.log('Got the Honcho User');
-
-  const honchoPayload = {
-    appId: honchoApp.id,
-    userId: honchoUser.id,
-    sessionId: conversationId,
-    userInput: message,
-    thought,
-  };
-
-  try {
-    let messages;
-    if (type === 'thought') {
-      messages = await thinkCall({
-        userInput: message,
-        appId: honchoApp.id,
-        userId: honchoUser.id,
-        sessionId: conversationId,
-      });
-    } else if (type === 'honcho') {
-      const dialecticQuery = await honcho.apps.users.sessions.chat(
-        honchoApp.id,
-        honchoUser.id,
-        conversationId,
-        { queries: thought }
-      );
-
-      return NextResponse.json({ content: dialecticQuery.content });
-    } else {
-      // @ts-expect-error - honchoContent is not defined in the type
-      honchoPayload['honchoContent'] = honchoThought;
-
-      messages = await respondCall({
-        userInput: message,
-        appId: honchoApp.id,
-        userId: honchoUser.id,
-        sessionId: conversationId,
-        honchoContent: honchoThought,
-      });
-    }
-
-    console.log('Getting the Stream');
-
-    const stream = await fetchOpenRouter(type, messages, honchoPayload);
-
-    if (!stream) {
-      throw new Error('Failed to get stream');
-    }
-
-    console.log('Got the Stream');
-
-    return new NextResponse(stream.body, {
-      status: 200,
-      headers: {
-        'Content-Type': 'text/event-stream',
-        'Cache-Control': 'no-cache',
-        Connection: 'keep-alive',
-      },
-    });
-  } catch (error) {
-    console.error('Stream error:', error);
-    return new NextResponse('Internal Server Error', { status: 500 });
   }
 }
