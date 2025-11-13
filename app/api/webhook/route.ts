5c2f92416c042fae89ef7b957b4076418b39c26f
Vineeth Voruganti <13438633+VVoruganti@users.noreply.github.com>
2025-11-13 11:40:57 -0500

    fix: Add specificity to stripe checkout flow

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -1,52 +1,52 @@
-import Stripe from 'stripe';
-import { headers } from 'next/headers';
+import Stripe from "stripe";
+import { headers } from "next/headers";
 import {
   upsertProductRecord,
   upsertPriceRecord,
   manageSubscriptionStatusChange,
   deleteProductRecord,
   deletePriceRecord,
-} from '@/utils/supabase/admin';
+} from "@/utils/supabase/admin";
 
 // Stripe Webhook events we want to track and take action against
 const relevantEvents = new Set([
-  'product.created',
-  'product.updated',
-  'product.deleted',
-  'price.created',
-  'price.updated',
-  'price.deleted',
-  'checkout.session.completed',
-  'customer.subscription.created',
-  'customer.subscription.updated',
-  'customer.subscription.deleted',
+  "product.created",
+  "product.updated",
+  "product.deleted",
+  "price.created",
+  "price.updated",
+  "price.deleted",
+  "checkout.session.completed",
+  "customer.subscription.created",
+  "customer.subscription.updated",
+  "customer.subscription.deleted",
 ]);
 
 export async function POST(req: Request) {
   if (!process.env.STRIPE_SECRET_KEY) {
     console.log(
-      'STRIPE_SECRET_KEY environment variable is not set, skipping webhook processing'
+      "STRIPE_SECRET_KEY environment variable is not set, skipping webhook processing",
     );
-    return new Response('Stripe not configured', { status: 200 });
+    return new Response("Stripe not configured", { status: 200 });
   }
 
   const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-    apiVersion: '2025-01-27.acacia',
+    apiVersion: "2025-01-27.acacia",
   });
 
   const body = await req.text();
-  const signature = (await headers()).get('Stripe-Signature') as string;
+  const signature = (await headers()).get("Stripe-Signature") as string;
   const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
 
   let event: Stripe.Event;
 
   try {
     if (!signature || !webhookSecret) {
-      return new Response('Webhook secret not found.', { status: 400 });
+      return new Response("Webhook secret not found.", { status: 400 });
     }
     event = stripe.webhooks.constructEvent(body, signature, webhookSecret);
   } catch (err: any) {
-    console.log('Webhook Error:', err.message);
+    console.log("Webhook Error:", err.message);
     return new Response(`Webhook Error: ${err.message}`, { status: 400 });
     // return NextResponse.json({ error: `Webhook Error: ${err.message}` }, { status: 400 });
   }
@@ -54,53 +54,59 @@ export async function POST(req: Request) {
   if (relevantEvents.has(event.type)) {
     try {
       switch (event.type) {
-        case 'product.created':
-        case 'product.updated':
-          console.log('Product');
+        case "product.created":
+        case "product.updated":
+          console.log("Product");
           await upsertProductRecord(event.data.object as Stripe.Product);
           break;
-        case 'price.created':
-        case 'price.updated':
-          console.log('Price');
+        case "price.created":
+        case "price.updated":
+          console.log("Price");
           await upsertPriceRecord(event.data.object as Stripe.Price);
           break;
-        case 'price.deleted':
+        case "price.deleted":
           await deletePriceRecord(event.data.object as Stripe.Price);
           break;
-        case 'product.deleted':
+        case "product.deleted":
           await deleteProductRecord(event.data.object as Stripe.Product);
           break;
-        case 'customer.subscription.created':
-        case 'customer.subscription.updated':
-        case 'customer.subscription.deleted':
+        case "customer.subscription.created":
+        case "customer.subscription.updated":
+        case "customer.subscription.deleted": {
           const subscription = event.data.object as Stripe.Subscription;
           await manageSubscriptionStatusChange(
             subscription.id,
             subscription.customer as string,
-            event.type === 'customer.subscription.created'
+            event.type === "customer.subscription.created",
           );
           break;
-        case 'checkout.session.completed':
+        }
+        case "checkout.session.completed": {
           const checkoutSession = event.data.object as Stripe.Checkout.Session;
-          if (checkoutSession.mode === 'subscription') {
+          const { metadata } = checkoutSession;
+          if (
+            metadata?.product === "bloom" &&
+            checkoutSession.mode === "subscription"
+          ) {
             const subscriptionId = checkoutSession.subscription;
             manageSubscriptionStatusChange(
               subscriptionId as string,
               checkoutSession.customer as string,
-              true
+              true,
             );
           }
           break;
+        }
         default:
           console.error(`Unhandled relevant event type ${event.type}`);
       }
     } catch (error) {
       console.log(error);
       return new Response(
-        'Webhook handler failed, view your Next.js function logs.',
+        "Webhook handler failed, view your Next.js function logs.",
         {
           status: 400,
-        }
+        },
       );
     }
   } else {
